6eecd5d78af26b1ecf5bf716eeb2173ea70c529e
Daniel <danvalleribeiro@gmail.com>
2026-05-07 16:22:20 -0300

    User Entity and Resource

--- src/main/java/com/yhr/course/entities/User.java
+++ src/main/java/com/yhr/course/entities/User.java
@@ -1,8 +1,12 @@
 package com.yhr.course.entities;
 
+import java.io.Serial;
+import java.io.Serializable;
 import java.util.Objects;
 
-public class User {
+public class User implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
     private Long id;
     private String name;
     private String email;
---
+++ src/main/java/com/yhr/course/resources/UserResource.java
@@ -0,0 +1,18 @@
+package com.yhr.course.resources;
+
+import com.yhr.course.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+    @GetMapping
+    public ResponseEntity<User> findAll()
+    {
+        User user = new User(1L, "Maria", "maria@gmail.com", "999999999", "12345");
+        return ResponseEntity.ok().body(user);
+    }
+}
