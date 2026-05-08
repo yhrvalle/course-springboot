8bcb49785e8f47e4780eaec489364eaeb22e35c4
Daniel <danvalleribeiro@gmail.com>
2026-05-08 16:21:40 -0300

    Order Status Enumeration

--- src/main/java/com/yhr/course/entities/Order.java
+++ src/main/java/com/yhr/course/entities/Order.java
@@ -1,6 +1,7 @@
 package com.yhr.course.entities;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.yhr.course.entities.enums.OrderStatus;
 import jakarta.persistence.*;
 
 import java.io.Serial;
@@ -17,6 +18,7 @@ public class Order implements Serializable {
     private Long id;
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
+    private Integer orderStatus;
 
     @ManyToOne
     @JoinColumn(name = "client_id")
@@ -26,10 +28,11 @@ public class Order implements Serializable {
 
     }
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
         this.id = id;
         this.moment = moment;
         this.client = client;
+        setOrderStatus(orderStatus);
     }
 
     public Long getId() {
@@ -46,6 +49,16 @@ public class Order implements Serializable {
 
     public void setMoment(Instant moment) {
         this.moment = moment;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if (orderStatus != null) {
+            this.orderStatus = orderStatus.getCode();
+        }
     }
 
     public User getClient() {
---
+++ src/main/java/com/yhr/course/entities/enums/OrderStatus.java
@@ -0,0 +1,27 @@
+package com.yhr.course.entities.enums;
+
+public enum OrderStatus {
+    WAITING_PAYMENT(1),
+    PAID(2),
+    SHIPPED(3),
+    DELIVERED(4),
+    CANCELED(5);
+    private final int code;
+
+    private OrderStatus(int code) {
+        this.code = code;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public static OrderStatus valueOf(int code) {
+        for (OrderStatus orderStatus : OrderStatus.values()) {
+            if (orderStatus.getCode() == code) {
+                return orderStatus;
+            }
+        }
+        throw new IllegalArgumentException("OrderStatus code " + code + " not found");
+    }
+}
